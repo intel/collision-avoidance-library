f011f834550a6fb65c04b6d6e154e3760237d9b2
Guilherme Campos Camargo <guilherme.campos.camargo@intel.com>
2016-10-26 17:22:19 -0200

    Fix identation
    Replace tabs with spaces and other minor changes.
    
    Signed-off-by: Guilherme Campos Camargo <guilherme.campos.camargo@intel.com>

--- testbed/testbed.sh
+++ testbed/testbed.sh
@@ -34,154 +34,154 @@ COAV_GCS_UDP_PORT=15557
 USE_PX4_AUTOPILOT=${USE_PX4_AUTOPILOT:-0}
 
 silentkill () {
-	if [ ! -z $2 ]; then
-		kill $2 $1 > /dev/null 2>&1 || true
-	else
-		kill -KILL $1 > /dev/null 2>&1 && wait $1 2> /dev/null || true
-	fi
+    if [ ! -z $2 ]; then
+        kill $2 $1 > /dev/null 2>&1 || true
+    else
+        kill -KILL $1 > /dev/null 2>&1 && wait $1 2> /dev/null || true
+    fi
 }
 
 test_dep () {
-	command -v $1 > /dev/null 2>&1 || { echo >&2 "Error: command '$1' not found"; exit 1; }
+    command -v $1 > /dev/null 2>&1 || { echo >&2 "Error: command '$1' not found"; exit 1; }
 }
 
 check_deps () {
-	test_dep gz
-	test_dep socat
-	test_dep $APM_CMD
+    test_dep gz
+    test_dep socat
+    test_dep $APM_CMD
 }
 
 test_path () {
-	[ -d $1 ] > /dev/null 2>&1 || { echo >&2 "Error: directory '$1' not found"; exit 1; }
+    [ -d $1 ] > /dev/null 2>&1 || { echo >&2 "Error: directory '$1' not found"; exit 1; }
 }
 
 check_paths () {
-	test_path $SCRIPT_DIR
-	if (("$USE_PX4_AUTOPILOT" == 1)); then
-		test_path $PX4_DIR
-	fi
+    test_path $SCRIPT_DIR
+    if (("$USE_PX4_AUTOPILOT" == 1)); then
+        test_path $PX4_DIR
+    fi
 }
 
 sleep_until_takeoff () {
-	gz topic -u -e /gazebo/default/gzsitl_quadcopter_rs/vehicle_pose \
-		| python detect_takeoff.py $1 # Detect takeoff on distance $1
+    gz topic -u -e /gazebo/default/gzsitl_quadcopter_rs/vehicle_pose \
+        | python detect_takeoff.py $1 # Detect takeoff on distance $1
 }
 
 listen_collision () {
-	SLEEPTIME=$1
-	TOPIC=/gazebo/default/gzsitl_quadcopter_rs/sensors/contact/contacts
-	gz topic -u -e $TOPIC | python detect_collision.py $SLEEPTIME
+    SLEEPTIME=$1
+    TOPIC=/gazebo/default/gzsitl_quadcopter_rs/sensors/contact/contacts
+    gz topic -u -e $TOPIC | python detect_collision.py $SLEEPTIME
 }
 
 testcase () {
-	WORLD=$1
-	SLEEPTIME=$2
+    WORLD=$1
+    SLEEPTIME=$2
 
-	# Create logdir if it does not exist
-	LOGDIR="${SCRIPT_DIR}/output/${1}"
-	mkdir -p $LOGDIR
+    # Create logdir if it does not exist
+    LOGDIR="${SCRIPT_DIR}/output/${1}"
+    mkdir -p $LOGDIR
 
-	# Run SITL Simulator
-	if (("$USE_PX4_AUTOPILOT" == 1)); then
-		cd $PX4_DIR
-		$PX4_CMD > "${LOGDIR}/sitl.log" \
-		    2> "${LOGDIR}/sitlerr.log" &
-		SITLID=$!
-		cd - > /dev/null
-	else
-		cd $SCRIPT_DIR # sitl must run in the same dir of "eeprom.bin"
-		$APM_CMD --model x \
-		    > "${LOGDIR}/sitl.log" \
-		    2> "${LOGDIR}/sitlerr.log" &
-		SITLID=$!
-		cd - > /dev/null
-	fi
+    # Run SITL Simulator
+    if (("$USE_PX4_AUTOPILOT" == 1)); then
+        cd $PX4_DIR
+        $PX4_CMD > "${LOGDIR}/sitl.log" \
+            2> "${LOGDIR}/sitlerr.log" &
+        SITLID=$!
+        cd - > /dev/null
+    else
+        cd $SCRIPT_DIR # sitl must run in the same dir of "eeprom.bin"
+        $APM_CMD --model x \
+            > "${LOGDIR}/sitl.log" \
+            2> "${LOGDIR}/sitlerr.log" &
+        SITLID=$!
+        cd - > /dev/null
+    fi
 
-	# Wait until gazebo is up and running
-	sleep 8
+    # Wait until gazebo is up and running
+    sleep 8
 
-	# Gazebo engine without GUI.
-	# The log can be played through `gazebo -p logfile`
-	SDFFILE="${SCRIPT_DIR}/worlds/${WORLD}"
-	gzserver --verbose -r --record_path $LOGDIR $SDFFILE \
-		> "${LOGDIR}/gzserver.log" \
-		2> "${LOGDIR}/gzservererr.log" &
-	GZID=$!
+    # Gazebo engine without GUI.
+    # The log can be played through `gazebo -p logfile`
+    SDFFILE="${SCRIPT_DIR}/worlds/${WORLD}"
+    gzserver --verbose -r --record_path $LOGDIR $SDFFILE \
+        > "${LOGDIR}/gzserver.log" \
+        2> "${LOGDIR}/gzservererr.log" &
+    GZID=$!
 
-	# Wait until gazebo is up and running
-	sleep 8
+    # Wait until gazebo is up and running
+    sleep 8
 
-	SOCAT_ARG_1="tcp:localhost:$APM_TCP_PORT_1"
-	SOCAT_ARG_2="udp:localhost:$GZSITL_UDP_PORT"
-	if (("$USE_PX4_AUTOPILOT" == 1)); then
-		SOCAT_ARG_1="udp-listen:$PX4_UDP_PORT_1"
-	fi
+    SOCAT_ARG_1="tcp:localhost:$APM_TCP_PORT_1"
+    SOCAT_ARG_2="udp:localhost:$GZSITL_UDP_PORT"
+    if (("$USE_PX4_AUTOPILOT" == 1)); then
+        SOCAT_ARG_1="udp-listen:$PX4_UDP_PORT_1"
+    fi
 
-	# Bidirectional bridge between autopilot and gazebo-sitl
-	socat $SOCAT_ARG_1 $SOCAT_ARG_2 \
-		> "${LOGDIR}/gzsitl_socat.log" \
-		2> "${LOGDIR}/gzsitl_socaterr.log" &
-	GZSITL_SOCATID=$!
+    # Bidirectional bridge between autopilot and gazebo-sitl
+    socat $SOCAT_ARG_1 $SOCAT_ARG_2 \
+        > "${LOGDIR}/gzsitl_socat.log" \
+        2> "${LOGDIR}/gzsitl_socaterr.log" &
+    GZSITL_SOCATID=$!
 
-	# Wait for gzsitl-sitl connection to be stabilished
-	sleep 2
+    # Wait for gzsitl-sitl connection to be stabilished
+    sleep 2
 
-	# Run the collision avoidance gcs
-	../build/samples/coav_gcs --depth-camera GAZEBO --vehicle GAZEBO \
-		> "${LOGDIR}/coav_gcs.log" \
-		2> "${LOGDIR}/coav_gcserr.log" &
-	COAVGCSID=$!
+    # Run the collision avoidance gcs
+    ../build/samples/coav_gcs --depth-camera GAZEBO --vehicle GAZEBO \
+        > "${LOGDIR}/coav_gcs.log" \
+        2> "${LOGDIR}/coav_gcserr.log" &
+    COAVGCSID=$!
 
-	# Wait until gcs is up and running
-	sleep 4
+    # Wait until gcs is up and running
+    sleep 4
 
-	SOCAT_ARG_1="tcp:localhost:$APM_TCP_PORT_2"
-	SOCAT_ARG_2="udp:localhost:$COAV_GCS_UDP_PORT"
-	if (("$USE_PX4_AUTOPILOT" == 1)); then
-		SOCAT_ARG_1="udp-listen:$PX4_UDP_PORT_2"
-	fi
+    SOCAT_ARG_1="tcp:localhost:$APM_TCP_PORT_2"
+    SOCAT_ARG_2="udp:localhost:$COAV_GCS_UDP_PORT"
+    if (("$USE_PX4_AUTOPILOT" == 1)); then
+        SOCAT_ARG_1="udp-listen:$PX4_UDP_PORT_2"
+    fi
 
-	# Bidirectional bridge between the autopilot and the coav_gcs
-	socat $SOCAT_ARG_1 $SOCAT_ARG_2 \
-		> "${LOGDIR}/coav_socat.log" \
-		2> "${LOGDIR}/coav_socaterr.log" &
-	COAV_SOCATID=$!
+    # Bidirectional bridge between the autopilot and the coav_gcs
+    socat $SOCAT_ARG_1 $SOCAT_ARG_2 \
+        > "${LOGDIR}/coav_socat.log" \
+        2> "${LOGDIR}/coav_socaterr.log" &
+    COAV_SOCATID=$!
 
-	# Wait for coav_gcs-SITL connection to be stabilished
-	sleep 2
+    # Wait for coav_gcs-SITL connection to be stabilished
+    sleep 2
 
-	sleep_until_takeoff 0.5 # Detect takeoff on a distance from origin >= 0.5 meters
+    sleep_until_takeoff 0.5 # Detect takeoff on a distance from origin >= 0.5 meters
 
-	# Maximum acceptable time for this particular mission
-	listen_collision $SLEEPTIME \
-		&& echo "[${WORLD}] OK!" || echo "[${WORLD}] FAIL!"
+    # Maximum acceptable time for this particular mission
+    listen_collision $SLEEPTIME \
+        && echo "[${WORLD}] OK!" || echo "[${WORLD}] FAIL!"
 
-	cleanup
+    cleanup
 }
 
 runtests () {
-	mkdir -p "${SCRIPT_DIR}/output"
+    mkdir -p "${SCRIPT_DIR}/output"
 
-	testcase simple.sdf 30
-	testcase simple_obstacle.sdf 40
+    testcase simple.sdf 30
+    testcase simple_obstacle.sdf 40
 }
 
 replay () {
-	gazebo -p "${SCRIPT_DIR}/output/${1}/state.log"
+    gazebo -p "${SCRIPT_DIR}/output/${1}/state.log"
 }
 
 cleanup () {
-	silentkill $GZSITL_SOCATID # Kill gzsitl socat
-	silentkill $COAV_SOCATID # Kill coav_gcs socat
-	silentkill $SITLID # Kill arducopter sitl
-	silentkill $GZID -INT && sleep 3 # Wait gzserver to save the log
-	silentkill $GZID  # Kill gzserver
-	silentkill $COAVGCSID # Kill coav_gcs sitl
+    silentkill $GZSITL_SOCATID # Kill gzsitl socat
+    silentkill $COAV_SOCATID # Kill coav_gcs socat
+    silentkill $SITLID # Kill arducopter sitl
+    silentkill $GZID -INT && sleep 3 # Wait gzserver to save the log
+    silentkill $GZID  # Kill gzserver
+    silentkill $COAVGCSID # Kill coav_gcs sitl
 }
 
 cleanup_and_exit () {
-	cleanup
-	exit 0
+    cleanup
+    exit 0
 }
 
 trap cleanup_and_exit SIGINT SIGTERM
@@ -190,8 +190,8 @@ check_deps
 check_paths
 
 if [ -z "$1" ]; then
-	# TODO: add help text
-	runtests # default subcommand
+    # TODO: add help text
+    runtests # default subcommand
 else
-	$@ # replay scene-name
+    $@ # replay scene-name
 fi
