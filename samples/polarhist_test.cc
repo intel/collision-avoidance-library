38fcf880a9044a6f83ed699b4a515aef3e2b55cb
Rodrigo Chiossi <rodrigo.chiossi@intel.com>
2017-01-12 22:44:56 +0100

    Add test sample for PolarHistogramDetector
    Simple visual test for PolarHistogramDetector used with RealSense
    camera. All scan slices are draw in the terminal, the ones with detected
    objects will be filled.

---
+++ samples/polarhist_test.cc
@@ -0,0 +1,63 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#include <cmath>
+#include <memory>
+#include <vector>
+#include <iostream>
+
+#include "common/common.hh"
+#include "detection/DepthImagePolarHistDetector.hh"
+#include "sensors/RealSenseCamera.hh"
+
+#define STEP (5.0 * M_PI / 180)
+
+int main(int argc, char **argv)
+{
+    std::shared_ptr<DepthCamera> depth_camera =
+        std::make_shared<RealSenseCamera>(640, 480, 30);
+
+    std::shared_ptr<DepthImagePolarHistDetector> obstacle_detector =
+        std::make_shared<DepthImagePolarHistDetector>(depth_camera, STEP, 1.5, 0.2);
+
+    double fov = atan(depth_camera->get_fov_tan());
+    unsigned int slices = ceil(fov / STEP);
+    double fixed_step = fov / slices;
+
+    std::cout << "fov = " << fov << std::endl;
+    std::cout << "step = " << fixed_step << std::endl;
+
+    while (true) {
+        std::vector<Obstacle> obstacles = obstacle_detector->detect();
+
+        std::vector<bool> visual_slices;
+        visual_slices.resize(slices, false);
+
+        for (Obstacle obs : obstacles) {
+            double offset = (M_PI / 2) - (fov / 2);
+            int pos = slices - (obs.spherical_pose.z - offset) / fixed_step;
+            visual_slices[pos] = true;
+        }
+
+        for (unsigned int i = 0; i < slices; i++)
+            std::cout << (visual_slices[i]?"████":"____");
+
+        std::cout << "\r";
+        std::cout.flush();
+    }
+
+    return 0;
+}
+
