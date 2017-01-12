4a120b9d02b4c34c6ba2cf60b60523e662c981cb
Rodrigo Chiossi <rodrigo.chiossi@intel.com>
2017-01-12 16:07:13 +0100

    Use Obstacle on SimpleDetector and StopAvoidance
    The use of boolean on Simple detector made it incompatible with other
    avoidance strategies other than StopAvoidance. Likewise, the use of
    boolean in StopAvoidance made it incompatible with other detection
    algorithms.

--- detection/DepthImageSimpleDetector.cc
+++ detection/DepthImageSimpleDetector.cc
@@ -14,6 +14,7 @@
 // limitations under the License.
 */
 #include <common/common.hh>
+#include <cstdint>
 #include <memory>
 #include <vector>
 #include <cmath>
@@ -33,7 +34,7 @@ DepthImageSimpleDetector::DepthImageSimpleDetector(
     this->threshold = threshold_m;
 }
 
-const std::vector<bool> &DepthImageSimpleDetector::detect()
+const std::vector<Obstacle> &DepthImageSimpleDetector::detect()
 {
     // Obtain camera depth buffer and camera properties
     std::vector<uint16_t> depth_buffer = this->sensor->get_depth_buffer();
@@ -41,10 +42,11 @@ const std::vector<bool> &DepthImageSimpleDetector::detect()
     unsigned int width = this->sensor->get_width();
     double scale = this->sensor->get_scale();
 
-    // Return false if depth buffer is empty
+    this->obstacles.clear();
+
+    // Return if depth buffer is empty
     if(depth_buffer.size() == 0) {
-        this->detection[0] = false;
-        return this->detection;
+        return this->obstacles;
     }
 
     // Detect obstacles in the center of the image
@@ -54,18 +56,34 @@ const std::vector<bool> &DepthImageSimpleDetector::detect()
     int final_j = fmin(width / 2 + defaults::radius_px, width);
 
     // Detect obstacles in the center of the image
-    this->detection[0] = false;
+    unsigned int min = UINT16_MAX;
     for (int i = init_i; i < final_i; i++) {
         for (int j = init_j; j < final_j; j++) {
             uint16_t depth_value = depth_buffer[i * width + j];
-            if (depth_value != 0 &&
-                depth_value < this->threshold / scale) {
-                this->detection[0] = true;
-                break;
+            if (depth_value == 0 ||
+                    depth_value >= this->threshold / scale) {
+                continue;
+            }
+
+            // If no obstacle was found yet, create a new one right in
+            // front of the vehicle. The distance will be set later.
+            if (obstacles.size() == 0) {
+                Obstacle obs = {0, glm::dvec3(0, 0, 0)};
+                this->obstacles.push_back(obs);
+            }
+
+            // save the minimum value
+            if (depth_value < min) {
+                min = depth_value;
             }
         }
     }
 
-    return this->detection;
+    // If an obstacle was found, set its distance to the minimum distance
+    if (obstacles.size() != 0) {
+        obstacles[0].center.x = (double) min * scale;
+    }
+
+    return this->obstacles;
 }
 
