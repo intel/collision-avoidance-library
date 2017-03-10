dfdb24e9cd1a4784774420324b1ee1b2111fadd1
Murilo Belluzzo <murilo.belluzzo@intel.com>
2017-03-10 13:56:40 -0800

    coav-sim: Exit on fail
    There is no reason in trying to proceed with the simulation script if
    any of the pieces fail to issue a command. Its better to quit.
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- testbed/coav-sim.sh
+++ testbed/coav-sim.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
 # Set autopilot
